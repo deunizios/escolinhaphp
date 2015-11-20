34448441de4c75a9406947755e8322663ee0ae25
deunizios <deunizios@gmail.com>
2015-11-20 00:39:37 -0200

    Criação do arquivo index.php

---
+++ nbproject/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1> Manuseio de Formulário PHP</h1>
+        <h2>Referências:</h2>
+        <ul>
+            <li><a href="http://www.w3schools.com/php/">W3Schools</a></li>
+            <li><a href="http://www.php.net">Manual do PHP</a></li>
+        </ul>
+        <?php
+        ?>
+    </body>
+</html>
